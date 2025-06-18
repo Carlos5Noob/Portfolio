2af1b5e3fab44711f36e395e31fcbfbdf949183a
Carlos5Noob <cchaati2101@g.educaand.es>
2025-06-18 17:07:46 +0200

    ADD: tool tailwind

--- src/components/Modules/Tools/index.tsx
+++ src/components/Modules/Tools/index.tsx
@@ -6,12 +6,14 @@ import django from '../../../assets/django.png';
 import git from '../../../assets/git.png';
 import html from '../../../assets/html.png';
 import react from '../../../assets/react.png';
+import tailwind from '../../../assets/tailwind.png'
 
 const Tools = () => {
   const lenguajes = [
     "Python + Django",
     "React + Next.js",
     "HTML, CSS, JS, TS, SASS",
+    "Tailwind",
     "Bootstrap",
     "GIT y Github",
     "Docker"
@@ -21,12 +23,13 @@ const Tools = () => {
     <>
         <h1 id='Lenguajes y Herramientas' className={`text-center my-4 ${styles.title}`}>&lt; Lenguajes y Herramientas /&gt;</h1>
         <div className="text-center text-light d-block">
-          <p>{lenguajes[0]}<Icon src={django} width={40} height={40} /></p>
-          <p>{lenguajes[1]}<Icon src={react} width={40} height={40} /></p>
+          <p>{lenguajes[0]}<Icon src={django} width={40} height={50} /></p>
+          <p>{lenguajes[1]}<Icon src={react} width={40} height={35} /></p>
           <p>{lenguajes[2]}<Icon src={html} width={100} height={40} /></p>
-          <p>{lenguajes[3]}<Icon src={bootstrap} width={40} height={40} /></p>
-          <p>{lenguajes[4]}<Icon src={git} width={40} height={40} /></p>
-          <p>{lenguajes[5]}<Icon src={docker} width={40} height={40} /></p>
+          <p>{lenguajes[3]}<Icon src={tailwind} width={40} height={30} /></p>
+          <p>{lenguajes[4]}<Icon src={bootstrap} width={40} height={40} /></p>
+          <p>{lenguajes[5]}<Icon src={git} width={40} height={40} /></p>
+          <p>{lenguajes[6]}<Icon src={docker} width={40} height={40} /></p>
         </div>
     </>
   );
