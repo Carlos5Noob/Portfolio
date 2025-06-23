419c6bcd44ead4db5158fbdbca46ac9e6a47d94d
Carlos5Noob <cchaati2101@g.educaand.es>
2025-06-23 15:58:29 +0200

    REFACTOR: diseño /Card

--- src/components/Modules/Projects/index.tsx
+++ src/components/Modules/Projects/index.tsx
@@ -4,6 +4,9 @@ import tierramedia from "../../../assets/tierramedia.png";
 import todo from "../../../assets/todo.png";
 import calculadora from "../../../assets/calculadora.png";
 import styles from "./Projects.module.css";
+import django from '../../../assets/django.png';
+import bootstrap from '../../../assets/bootstrap.png';
+import tailwind from '../../../assets/tailwind.png';
 
 const Projects = () => {
   return (
@@ -17,14 +20,22 @@ const Projects = () => {
           src={critikon}
           href="https://github.com/Carlos5Noob/Critikon-project"
           title="Critikon"
-          text="Aplicación web pensada para gamers, quienes pueden discutir entre diversos videojuegos. Desarrollado con Python + Django."
+          text="Aplicación web pensada para gamers, quienes pueden discutir entre diversos videojuegos."
+          icon1="https://cdn-icons-png.flaticon.com/512/5968/5968350.png"
+          icon2={django}
+          icon3={bootstrap}
+          icon4="https://cdn-icons-png.flaticon.com/512/5968/5968342.png"
         />
         <Card
           className={styles.card}
           src={tierramedia}
           href="https://github.com/Carlos5Noob/TierraMedia-Django"
           title="Tierra Media"
-          text="Videojuego basado en las tierras intermedias del Señor de los Anillos. Desarrollado con Python + Django."
+          text="Videojuego basado en las tierras intermedias del Señor de los Anillos."
+          icon1="https://cdn-icons-png.flaticon.com/512/5968/5968350.png"
+          icon2={django}
+          icon3={bootstrap}
+          icon4="https://cdn-icons-png.flaticon.com/512/5968/5968342.png"
         />
       </div>
       <div className="container d-flex justify-content-center gap-5 mb-5">
@@ -33,14 +44,20 @@ const Projects = () => {
           src={todo}
           href="https://github.com/Carlos5Noob/to-do"
           title="TO-DONOOB"
-          text="Aplicación web para la gestión de tareas. Desarrollado con React + Tailwind."
+          text="Aplicación web para la gestión de tareas."
+          icon1="https://cdn-icons-png.flaticon.com/512/5968/5968381.png"
+          icon2="https://cdn-icons-png.flaticon.com/512/1126/1126012.png"
+          icon3={tailwind}
         />
         <Card
           className={styles.card}
           src={calculadora}
           href="https://github.com/Carlos5Noob/calculadora"
           title="CALC-U-NOOB"
-          text="Aplicación web que simula una calculadora de iOS. Desarrollado con React + Tailwind."
+          text="Aplicación web que simula una calculadora de iOS."
+          icon1="https://cdn-icons-png.flaticon.com/512/5968/5968381.png"
+          icon2="https://cdn-icons-png.flaticon.com/512/1126/1126012.png"
+          icon3={tailwind}
         />
       </div>
     </>
--- src/components/Utils/Card/index.tsx
+++ src/components/Utils/Card/index.tsx
@@ -1,28 +1,43 @@
 import Image from "../Image";
-import styles from './Card.module.css'
+import styles from "./Card.module.css";
+import Icon from "../Icon";
 
 type CardProps = {
-    src: string;
-    title: string;
-    text: string;
-    href: string;
-    alt?: string;
-    className?: string;
-}
+  src: string;
+  title: string;
+  text: string;
+  href: string;
+  icon1?: string;
+  icon2?: string;
+  icon3?: string;
+  icon4?: string;
+  alt?: string;
+  className?: string;
+};
 
 const Card = (props: CardProps) => {
-    const { href, src, title, text, alt, className } = props;
-    
-    return (
-        <div className={`card ${styles.container} ${className}`}>
-            <Image src={src} className={`card-img-top ${styles.imagen}`} alt={alt}/>
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{text}</p>
-                <a href={href} className="btn btn-primary">Visita el repositorio</a>
-            </div>
+  const { href, src, title, text, alt, className, icon1, icon2, icon3, icon4 } = props;
+
+  return (
+    <div className={`card ${styles.container} ${className}`}>
+      <Image src={src} className={`card-img-top ${styles.imagen}`} alt={alt} />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{text}</p>
+        <div className="d-flex">
+          <a href={href} className="btn btn-primary">
+            Visita el repositorio
+          </a>
+          <div className="ms-auto">
+            {icon1 && <Icon width={30} height={30} src={icon1} />}
+            {icon2 && <Icon width={30} height={30} src={icon2} />}
+            {icon3 && <Icon width={30} height={30} src={icon3} />}
+            {icon4 && <Icon width={30} height={30} src={icon4} />}
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Card;
